6b89ee22cfe721268735d8fd630e27333639ac5e
Damien-Senechal <60384944+Damien-Senechal@users.noreply.github.com>
2024-12-17 17:46:54 +0100

    Implement user story 2 code

--- routes/stock_routes.py
+++ routes/stock_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, json, request, jsonify
 from models.database import db
 from models.stock import Stock
 
@@ -11,3 +11,10 @@ def get_stocks():
         return jsonify([stock.to_dict() for stock in stocks]), 200
     except Exception as e:
         return jsonify({"error": "Erreur lors de la récupération des stocks"}), 500
+
+@stock_routes.route("/stocks/<int:stock_id>", methods=["GET"])
+def get_stock(stock_id):
+    stock = db.session.get(Stock, stock_id)
+    if not stock:
+        return jsonify({"error": "Stock not found"}), 404
+    return jsonify(stock.to_dict()), 200
